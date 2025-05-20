46672e0ec3a0e7c3a22f925db07082d737e5e74c
Thiago Sousa <thi_ago5@hotmail.com>
2025-05-19 22:37:52 -0300

    :sparkles: feature: integrando lista e adicionando itens.

--- curso-angular/src/app/lista-compras/lista-compras.component.ts
+++ curso-angular/src/app/lista-compras/lista-compras.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ItemLista } from './item-lista';
 
 @Component({
   selector: 'app-lista-compras',
@@ -11,11 +12,21 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 })
 export class ListaComprasComponent {
 
-    form: FormGroup = new FormGroup({
-      itemList: new FormControl('', Validators.required)
-    });
+  item: string = '';
+  list: ItemLista[] = []
+
+  form: FormGroup = new FormGroup({
+    itemList: new FormControl('', Validators.required)
+  });
+
 
   submit(): void {
+    const itemList = new ItemLista();
+    itemList.name = this.form.value.itemList;
+    itemList.id = this.list.length + 1;
+
+    this.list.push(itemList);
+    this.form.reset();
   }
 
 }
